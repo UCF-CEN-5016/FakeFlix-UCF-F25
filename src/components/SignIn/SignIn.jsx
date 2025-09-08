feca355dd4979b788506dffce49225676c85303d
Kevin Pereda <kptwdo15@gmail.com>
2025-09-08 15:25:20 -0400

    Added documentation in the form of JSDoc for the SignIn function which is the main component SignIn.jsx is serving

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,93 +1,123 @@
-import './signIn.scss';
+import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
+import { useDispatch, useSelector } from "react-redux";
+import { authFadeInUpVariants, staggerOne } from "../../motionUtils";
+import {
+  anonymousSignInStart,
+  emailSignInStart,
+  googleSignInStart,
+} from "../../redux/auth/auth.actions";
+import { selectAuthLoadingState } from "../../redux/auth/auth.selectors";
 import InputField from "../InputField/InputField";
 import Loader from "../Loader/Loader";
-import { motion } from "framer-motion";
-import { authFadeInUpVariants, staggerOne } from "../../motionUtils";
-import { FcGoogle } from "react-icons/fc";
-import { useForm } from "react-hook-form";
-import { emailSignInStart, googleSignInStart, anonymousSignInStart } from "../../redux/auth/auth.actions";
-import { useDispatch, useSelector } from "react-redux";
-import { selectAuthLoadingState } from "../../redux/auth/auth.selectors";
+import "./signIn.scss";
 
+/**
+ * SignIn - This is the main component in this file that when called elsewhere in our codebase will render the sign-in form for users to see
+ *
+ * Creates a form with password/email inputs and three sign in options:
+ * - Email / password
+ * - Google sign in
+ * - Anonymous sign in
+ *
+ * Uses React Hook Form for easier form management(register, handleSubmit), Redux for state management,
+ * and Framer Motion for animations.
+ *
+ * @returns {JSX.Element} Animated form with input fields for user sign in information and sign in buttons
+ */
 const SignIn = () => {
-	const dispatch = useDispatch();
-	const isLoading = useSelector(selectAuthLoadingState);
-	const { register, handleSubmit, errors } = useForm({
-		mode: "onTouched"
-	})
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectAuthLoadingState);
+  const { register, handleSubmit, errors } = useForm({
+    mode: "onTouched",
+  });
 
-	const onSubmit = data => {
-		const { email, password } = data;
-		dispatch(emailSignInStart({ email, password }));
-	}
+  const onSubmit = (data) => {
+    const { email, password } = data;
+    dispatch(emailSignInStart({ email, password }));
+  };
 
-	return (
-		<motion.form
-			variants={staggerOne}
-			initial="initial"
-			animate="animate"
-			exit="exit"
-			className="SignIn__form"
-			onSubmit={handleSubmit(onSubmit)}
-		>
-			<motion.div variants={authFadeInUpVariants} className="SignIn__form--inputwrp">
-				<InputField
-					type="text"
-					name="email"
-					placeholder="E-mail"
-					validationMessage="Please enter a valid email address."
-					validation={register({
-						required: true,
-						pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-					})}
-					errors={errors}
-					disabled={isLoading}
-				/>
-			</motion.div>
-			<motion.div variants={authFadeInUpVariants} className="SignIn__form--inputwrp">
-				<InputField
-					type="password"
-					name="password"
-					placeholder="Password"
-					validationMessage="The password should have a length between 6 and 30 characters."
-					validation={register({
-						required: true,
-						minLength: 6,
-						maxLength: 30,
-					})}
-					errors={errors}
-					disabled={isLoading}
-				/>
-			</motion.div>
-			<motion.button
-				type="submit"
-				variants={authFadeInUpVariants}
-				className={`SignIn__form--button button__submit ${isLoading && 'loading'}`}
-				disabled={isLoading}
-			>
-				{isLoading ? <Loader /> : 'Sign in'}
-			</motion.button>
-			<motion.button
-				type="button"
-				variants={authFadeInUpVariants}
-				className={`SignIn__form--button button__google ${isLoading && 'loading'}`}
-				onClick={() => dispatch(googleSignInStart())}
-				disabled={isLoading}
-			>
-				{!isLoading && <FcGoogle />}
-				{isLoading ? <Loader /> : 'Sign in with Google'}
-			</motion.button>
-			<motion.button
-				type="button"
-				variants={authFadeInUpVariants}
-				className={`SignIn__form--button button__anonymous ${isLoading && 'loading'}`}
-				onClick={() => dispatch(anonymousSignInStart())}
-				disabled={isLoading}
-			>
-				{isLoading ? <Loader /> : 'Sign in anonymously'}
-			</motion.button>
-		</motion.form>
-	)
-}
+  return (
+    <motion.form
+      variants={staggerOne}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      className="SignIn__form"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <motion.div
+        variants={authFadeInUpVariants}
+        className="SignIn__form--inputwrp"
+      >
+        <InputField
+          type="text"
+          name="email"
+          placeholder="E-mail"
+          validationMessage="Please enter a valid email address."
+          validation={register({
+            required: true,
+            pattern:
+              /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+          })}
+          errors={errors}
+          disabled={isLoading}
+        />
+      </motion.div>
+      <motion.div
+        variants={authFadeInUpVariants}
+        className="SignIn__form--inputwrp"
+      >
+        <InputField
+          type="password"
+          name="password"
+          placeholder="Password"
+          validationMessage="The password should have a length between 6 and 30 characters."
+          validation={register({
+            required: true,
+            minLength: 6,
+            maxLength: 30,
+          })}
+          errors={errors}
+          disabled={isLoading}
+        />
+      </motion.div>
+      <motion.button
+        type="submit"
+        variants={authFadeInUpVariants}
+        className={`SignIn__form--button button__submit ${
+          isLoading && "loading"
+        }`}
+        disabled={isLoading}
+      >
+        {isLoading ? <Loader /> : "Sign in"}
+      </motion.button>
+      <motion.button
+        type="button"
+        variants={authFadeInUpVariants}
+        className={`SignIn__form--button button__google ${
+          isLoading && "loading"
+        }`}
+        onClick={() => dispatch(googleSignInStart())}
+        disabled={isLoading}
+      >
+        {!isLoading && <FcGoogle />}
+        {isLoading ? <Loader /> : "Sign in with Google"}
+      </motion.button>
+      <motion.button
+        type="button"
+        variants={authFadeInUpVariants}
+        className={`SignIn__form--button button__anonymous ${
+          isLoading && "loading"
+        }`}
+        onClick={() => dispatch(anonymousSignInStart())}
+        disabled={isLoading}
+      >
+        {isLoading ? <Loader /> : "Sign in anonymously"}
+      </motion.button>
+    </motion.form>
+  );
+};
 
 export default SignIn;
