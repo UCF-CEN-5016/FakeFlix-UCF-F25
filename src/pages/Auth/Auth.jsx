34a19f443a599caa4faad39f4caec6fca73c49be
Anthony Vargas <90121982+Speedrunyourknowledge@users.noreply.github.com>
2025-09-10 15:46:41 -0400

    Add comments to document Auth.jsx logic

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -9,11 +9,15 @@ import { LOGO_URL, SIGNIN_BGIMG_URL } from "../../requests.js";
 import { useSelector } from "react-redux";
 import { selectAuthErrors } from "../../redux/auth/auth.selectors";
 
+// Auth page for sign in and sign up views
 const Auth = () => {
+  // State to toggle between sign in and sign up
 	const [isSignedUp, setIsSignedUp] = useState(true);
+  // Get authentication errors from Redux store
 	const authError = useSelector(selectAuthErrors);
 
 	return (
+    // Main container with page transition animations
 		<motion.div
 			className="Auth"
 			variants={authPageFadeInVariants}
@@ -21,11 +25,15 @@ const Auth = () => {
 			animate="animate"
 			exit="exit"
 		>
+      {/* Opacity effect */}
 			<div className="Auth__opacityLayer" />
+      {/* Background image */}
 			<div className="Auth__bgLayer" style={{ backgroundImage: `url(${SIGNIN_BGIMG_URL})` }} />
+      {/* Logo linking to home */}
 			<Link to="/" className="Auth__logo">
 				<img className="Auth__logo--img" src={LOGO_URL} alt="Fakeflix_logo" />
 			</Link>
+      {/* Handles entrance/exit for the entire auth content */}
 			<motion.div
 				className="Auth__content"
 				variants={modalVariants}
@@ -33,16 +41,23 @@ const Auth = () => {
 				animate="visible"
 				exit="hidden"
 			>
+        {/* Staggered animations for child elements */}
 				<motion.div variants={staggerOne} initial="initial" animate="animate" exit="exit">
+          {/* Title fades in and up */}
 					<motion.h2 variants={authFadeInUpVariants} className="Auth__content--title">
 						{isSignedUp ? "Sign In" : "Sign Up"}
 					</motion.h2>
+          {/* Conditional rendering of top text */}
 					<motion.small variants={authFadeInUpVariants} className="Auth__content--disclaimer">
 						{`Pay attention: this is not the original Netflix ${isSignedUp ? "sign in" : "sign up"}. Don't insert your real credentials here!`}
 					</motion.small>
+          {/* Conditional rendering of SignIn or SignUp component */}
 					{isSignedUp ? <SignIn /> : <SignUp />}
+          {/* Display authentication errors if any */}
 					{authError && <motion.p variants={authFadeInUpVariants} className='Auth__content--errors'>{authError}</motion.p>}
+          {/* Divider line */}
 					<motion.hr variants={authFadeInUpVariants} className="Auth__content--divider" />
+          {/* Conditional rendering of bottom text */}
 					<motion.small variants={authFadeInUpVariants} className="Auth__content--toggleView">
 						{isSignedUp
 							? `Haven't you registered yet? `
