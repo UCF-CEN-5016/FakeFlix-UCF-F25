cc6aeed0337c3264ffe225308041b99c06fcb1b8
wrmoulton <williamrmoulton@icloud.com>
2025-09-10 23:29:46 -0400

    docs: add documentation for PlayAnimation component

--- src/components/PlayAnimation/PlayAnimation.jsx
+++ src/components/PlayAnimation/PlayAnimation.jsx
@@ -1,3 +1,10 @@
+//PlayAnimation.jsx
+
+/** 
+	Splash screen component.
+	On mount, it plays the Netflix sound, shows the FAKEFLIX logo, and redirects the user to the browse page after 4.2 seconds.
+**/
+
 import "./playAnimation.scss"
 import { useEffect, useRef } from "react";
 import { useHistory } from "react-router-dom";
@@ -6,12 +13,24 @@ import { TADUM_SOUND_URL } from "../../requests";
 const PlayAnimation = () => {
 
 	let history = useHistory();
+
+	//Reference to the audio var that will play the Netflix sound
 	const soundRef = useRef(null);
+
+	/**
+		Plays the ta-dum sound from the beginning.
+		Ensures the audio starts fresh each time this is triggered.
+	**/
 	const handleTadum = () => {
 		soundRef.current.currentTime = 0;
 		soundRef.current.play();
 	}
 
+	/**
+		useEffect hook runs once on component mount
+	 	-Plays the ta-dum audio
+	 	-After 4200ms, navigates user to the /browse route
+	**/
 	useEffect(() => {
 		handleTadum();
 		setTimeout(() => {
