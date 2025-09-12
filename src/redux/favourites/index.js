4fef33ca49c29a5ba826f476e536c1011e4d102f
PlasticRosewood <jdtomjones@gmail.com>
2025-09-12 14:49:02 -0400

    Added documentation to src/redux/favourites/index.js. Explained the behavior of the adding/removing favorites function.

--- src/redux/favourites/index.js
+++ src/redux/favourites/index.js
@@ -1,10 +1,21 @@
+/*
+    This file handles the removal and adding of 
+    media to the user's personal favorites list.
+*/
+
 import { favouritesActionTypes } from "./favourites.types"
 import { addToFavouritesUtil, removeFromFavouritesUtil } from "./favourites.utils"
 
+// initialize the user's list of favorites to be an empty array
 const initialState = {
     favouritesList: []
 }
 
+// accept as input the current state of the user's favorite list (initialize as empty)
+// accept as input the action (add  or  remove) that the user wants to perform
+// if action is add, append the payload to state array
+// if action is remove, remove payload from state array
+// otherwise, return state array as is
 const favouritesReducer = (state = initialState, action) => {
     switch (action.type) {
         case favouritesActionTypes.ADD_TO_FAVOURITES:
