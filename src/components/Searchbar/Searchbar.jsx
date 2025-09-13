375da508f708e6fdde1811318f2ab64bb32caf8d
Andry Canel <canelbandry@gmail.com>
2025-09-13 18:01:55 -0400

    docs(Searchbar): document most of the handlers like toggle and clear

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,11 +1,11 @@
-import "./searchbar.scss"
 import { useRef, useState } from "react";
-import { useHistory } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { changeSearchInputValue, clearSearchInputValue, fetchSearchResultsAsync } from "../../redux/search/search.actions";
 import { FiSearch } from "react-icons/fi";
 import { RiCloseFill } from "react-icons/ri";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 import useOutsideClick from "../../hooks/useOutsideClick";
+import { changeSearchInputValue, clearSearchInputValue, fetchSearchResultsAsync } from "../../redux/search/search.actions";
+import "./searchbar.scss";
 
 const Searchbar = () => {
 
@@ -23,18 +23,29 @@ const Searchbar = () => {
         }
     });
 
+    // When the magnifying glass is clicked:
+    // - focus the input
+    // - show/hide the input box
     const handleSearchInputToggle = () => {
         searchInputRef.current.focus();
         setSearchInputToggle(!searchInputToggle);
     };
 
+    // When the “X” is clicked:
+    // - clear the text (both local and in Redux)
+    // - go back to the Browse page
     const clearSearchInputToggle = () => {
         setSearchInput("");
         dispatch(clearSearchInputValue());
         history.push("/browse");
     };
 
-    const handleSearchInput = event => {
+    // Every time the user types:
+    // - save the new value locally
+    // - save it to Redux
+    // - if there’s text, go to /search?q=... and fetch results
+    // - if empty, go back to /browse
+    const handleSearchInput = (event) => {
         const { value } = event.target;
         setSearchInput(value);
         dispatch(changeSearchInputValue(value));
@@ -42,9 +53,10 @@ const Searchbar = () => {
         if (value.length > 0) {
             history.push(`/search?q=${value}`);
             dispatch(fetchSearchResultsAsync(value));
-        } else history.push("/browse");
+        } else {
+            history.push("/browse");
+        }
     };
-
     return (
         <div className="Searchbar" ref={searchbarRef}>
             <input
